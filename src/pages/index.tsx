e1e1d6dc08fec557660f8418223b03a1a15c4c26
Martin Ivanko <martinivanko1238@gmail.com>
2022-08-12 15:16:18 +0200

    start working at map component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import Map from "@/stories/components/Map";
 import type { NextPage } from "next";
+import React from "react";
 
 const Home: NextPage = () => {
-    return <p>hp</p>;
+    return (
+        <React.Fragment>
+            <Map />
+        </React.Fragment>
+    );
 };
 
 export default Home;
--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export interface ICard {
-    title: string;
-    desc: string;
-    imgSrc: string;
-    imgAlt: string;
-}
